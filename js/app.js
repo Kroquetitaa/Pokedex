190c4c33d62332824bd900797f32be660afba280
Kroquetitaa <Kroquetita@gmail.com>
2022-07-27 03:43:08 +0200

    pokedex

---
+++ js/app.js
@@ -0,0 +1,57 @@
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
+// const buttonPrev = document.querySelector('.btn-prev');
+// const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+
+const fetchPokemon = async (pokemon) => {
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  if (APIResponse.status === 200) {
+    const data = await APIResponse.json();
+    return data;
+  }
+}
+
+const renderPokemon = async (pokemon) => {
+  pokemonName.innerHTML = 'Cargando...';
+  pokemonNumber.innerHTML = '';
+  const data = await fetchPokemon(pokemon);
+
+  if (data) {
+    pokemonImage.style.display = 'block';
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    input.value = '';
+    searchPokemon = data.id;
+  } else {
+    pokemonImage.style.display = '';
+    pokemonName.innerHTML = '';
+    pokemonNumber.innerHTML = '';
+  }
+
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  renderPokemon(input.value.toLowerCase());
+});
+
+const buttonPrev = () => {
+    if (searchPokemon > 1) {
+            searchPokemon -= 1;
+            renderPokemon(searchPokemon);
+        }
+}
+
+const buttonNext = () => {
+    searchPokemon += 1;
+    renderPokemon( searchPokemon );
+}
+
+renderPokemon(searchPokemon);
